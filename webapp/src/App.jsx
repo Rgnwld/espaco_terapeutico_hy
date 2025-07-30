64de3b4ab08327d0c241891f0e95b7d8980827c7
Reginaldo <regifilho06@gmail.com>
2025-07-30 13:30:05 -0300

    init webapp

---
+++ webapp/src/App.jsx
@@ -0,0 +1,16 @@
+import { Alert } from 'react-bootstrap';
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Project_Routes from './routes';
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Project_Routes/>
+    </>
+  )
+}
+
+export default App
---
+++ webapp/src/assets/components/Header.component.jsx
@@ -0,0 +1,12 @@
+import { Button } from 'react-bootstrap';
+
+function Header() {
+  return (
+    <header>
+        <p>Teste Header</p>
+        <Button variant="primary">Click Me</Button>
+    </header>
+  )
+}
+
+export default Header
